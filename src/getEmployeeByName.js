a24478fb6dcd25c41642b566fd6992889fa5fbf7
Viviane Puga <vnpuga@gmail.com>
2022-01-25 18:02:29 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,17 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) return {};
+  return (
+    employees.find((employee) => employee.firstName === employeeName || employee.lastName === employeeName)
+    );
 }
+console.log(getEmployeeByName());
+console.log(getEmployeeByName('Emery'));
+
+// se não tiver nada no parâmetro, returna objeto vazio {}
+// usei find no objeto para encontrar o elemento de acordo com a condicional
 
 module.exports = getEmployeeByName;
