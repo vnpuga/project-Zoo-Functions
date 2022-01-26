03601ff069fa10f6a52fd3f0e207845e08feed98
Viviane Puga <vnpuga@gmail.com>
2022-01-26 09:20:52 -0300

    corrigindo eslint

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -4,9 +4,8 @@ const data = require('../data/zoo_data');
 function getEmployeeByName(employeeName) {
   // seu código aqui
   if (!employeeName) return {};
-  return (
-    employees.find((employee) => employee.firstName === employeeName || employee.lastName === employeeName)
-    );
+  return employees.find((employee) => employee
+    .firstName === employeeName || employee.lastName === employeeName);
 }
 console.log(getEmployeeByName());
 console.log(getEmployeeByName('Emery'));
