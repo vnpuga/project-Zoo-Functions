0ba2888c1cd0d2481d12319d60a8cf3ef246f095
Viviane Puga <vnpuga@gmail.com>
2022-01-27 16:36:33 -0300

    requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,19 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  // encontro o funcionário correspondente ao parâmetro, e o 1o animal pelo qual ele é responsável;
+  const findEmployee = employees.find((employee) => employee.id === id).responsibleFor[0];
+  // com id do animal, uso find para retornar o array residents;
+  const findAnimal = species.find((specie) => specie.id === findEmployee).residents;
+  // usando o sort() para ordenar em ordem decrescente pela idade(do mais velho p/ o mais novo)
+  const oldestAnimal = findAnimal.sort((a, b) => b.age - a.age);
+  // com o map(), crio a array trazendo apenas os valores de cada chave;
+  const resultado = oldestAnimal.map((element) => [element.name, element.sex, element.age]);
+  // por fim, retorno apenas o 1o elemento desse array, onde está o animal mais velho;
+  return resultado[0];
 }
+
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1')); // Nigel
 
 module.exports = getOldestFromFirstSpecies;
